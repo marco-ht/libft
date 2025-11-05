dc73ebeb2d628d9e7f5186f3b6e9f79ee0a4fdf7
marco-ht <marco-ht@outlook.it>
2025-11-05 22:05:17 +0100

    v.0

---
+++ ft_lstnew_bonus.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mpierant <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/20 16:09:21 by mpierant          #+#    #+#             */
+/*   Updated: 2024/12/20 16:09:40 by mpierant         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdlib.h>
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*ptr;
+
+	ptr = (t_list *)malloc(sizeof(t_list));
+	if (ptr == NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	ptr->content = content;
+	ptr->next = NULL;
+	return (ptr);
+}
